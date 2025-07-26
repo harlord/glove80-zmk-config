80d2918029869fc26c04dc1c0186f1e71819bbea
harlord <haroldhernandezt@gmail.com>
2025-07-26 13:34:58 -0500

    inital module

--- config/default.nix
+++ config/default.nix
@@ -5,9 +5,8 @@
 let
   config = ./.
   ;
-  extraModules = [ "${config}/modules/combos-lead" ];
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; modules = extraModules; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; modules = extraModules; };
+  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
 
 in firmware.combine_uf2 glove80_left glove80_right
