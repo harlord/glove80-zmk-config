289191940576b72ce0ddf93eba52d166336c4ee5
harlord <haroldhernandezt@gmail.com>
2025-07-26 13:41:21 -0500

    inital module

--- config/default.nix
+++ config/default.nix
@@ -4,9 +4,22 @@
 
 let
   config = ./.
-  ;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  # path to the combos-lead module checked out by the workflow
+  combosLead = "${config}/modules/combos-lead";
+
+  glove80_left = firmware.zmk.override {
+    board = "glove80_lh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+    extraModules = [ combosLead ];
+  };
+
+  glove80_right = firmware.zmk.override {
+    board = "glove80_rh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+    extraModules = [ combosLead ];
+  };
 
 in firmware.combine_uf2 glove80_left glove80_right
